97b3e55692db13a1fe758dd03e21ecf723ca24be
rizalabdullah <ra160920@gmail.com>
2022-02-15 12:35:18 +0700

    komponen reactjs

---
+++ src/LatihanReact/Komponen/FunctionalComponent.jsx
@@ -0,0 +1,11 @@
+const FunctionalComponent = ({nama}) => {
+
+	return(
+			<div>
+			 <h1>Membuat komponen dengan functional komponen</h1>
+            <h2>Hello {nama}</h2>
+			</div>
+	)
+}
+
+export default FunctionalComponent;
