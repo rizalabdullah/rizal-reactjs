34eefab99b13ac0debd62b893510daa3dc688f57
rizalabdullah <ra160920@gmail.com>
2022-02-22 19:26:15 +0700

    .

--- src/LatihanReact/Komponen/ClassComponent.jsx
+++ src/LatihanReact/Komponen/ClassComponent.jsx
@@ -2,72 +2,56 @@ import React from "react";
 import $ from "jquery";
 import { click } from "@testing-library/user-event/dist/click";
 
-const Article = ({data1}) => {
-    return(
-        <ul>
-           
-                {
-                data1.map((data11, key) => {
-                return(
-                   <li key={key}>{data11.title}</li>
-   
-      
-                    
-                )  
-                })
-                
-                }
-          
-
-        </ul>
-    )
-}
 
 class ClassComponent extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-		  data1: [],
-		  isLoaded: false
-	  } ;
-	  }
-	  
-	   
-	componentDidMount(){
-	   
-	  
-	
-	
-		fetch(`https://newsapi.org/v2/top-headlines?country=id&apiKey=a4c6db0fc6814847adf5743f510599a6`)
-		.then(response => response.json())
-	   .then((data) => {
-		  this.setState({
-			  isLoaded: true,
-			  data1: data.articles
-			})
+ constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://newsapi.org/v2/top-headlines?country=id&apiKey=a4c6db0fc6814847adf5743f510599a6")
+      .then(res => res.json())
+      .then(
+        (data) => {
+          this.setState({
+            isLoaded: true,
+            items: data.articles
+			
+          });
 		  console.log(data.articles)
-	  
-			
-	 
-	   
-		})
-	
-	
-	}
-		render(){
-			   
-			return(
-				<div>
-				dfdf
-				
-				
-				{<Article data1={this.state.data1} />}
-				 
-	
-				</div>
-			)
-			
-		}
-	}
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const {items } = this.state;
+  
+      return (
+        <ul>
+          {items.map((item,i) => {
+        return(    <li key={i}>
+              {item.title}
+            </li> )
+          })
+		 
+		
+		} 
+        </ul>   
+      );
+   
+  }
+}
 
 export default ClassComponent;
